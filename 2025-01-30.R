0df37ba355ca4e01c01a4d621f8fc90b5791c720
EF237 <jjamesrollins237@utexas.edu>
2025-01-30 16:59:20 -0600

    class work for today

---
+++ 2025-01-30.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+
+f <- "https://raw.githubusercontent.com/difiore/ada-datasets/refs/heads/main/KamilarAndCooperData.csv"
+
+d <- read_csv(f, col_names = TRUE) # creates a "tibble"
+
+mean(d$Brain_Size_Species_Mean, na.rm = TRUE)
+
+attach(d)
+
+mean(Brain_Size_Species_Mean, na.rm = TRUE)
+
+detach(d)
+
+with(d, 
+     mean(Brain_Size_Species_Mean, na.rm = TRUE)
+     detach(d)
+     with(
+       d,
+       mean(Body_mass_male_mean, na.rm = TRUE)
+     ))
+
+summary(d)
+
+install.packages("skimr")
+library(skimr)
+skim(d)
+
+boxplot(log(d$Body_mass_female_mean))
+stripchart(log(d$Body_mass_female_mean), 
+           method = "jitter",
+           col = "blue",
+           vertical = TRUE,
+           add = TRUE)
+
+boxplot(log(d$Body_mass_female_mean) ~ Family)
+stripchart(log(d$Body_mass_female_mean) ~
+             Family, 
+           method = "jitter",
+           col = "blue",
+           vertical = TRUE,
+           add = TRUE)
+
+# ggplot (data, mapping, aes, geom, theme)
+
+p <- ggplot(
+  data = d,
+  aes(x = "", y = log(Body_mass_female_mean))
+)+
+  geom_boxplot(na.rm = TRUE)
+
+p <- p + geom_boxplot(na.rm = TRUE)
+
+p <- p + geom_jitter(
+  color = "blue",
+  width = 0.5
+)
+
+p
+           
