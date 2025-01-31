d6ba71f2e3eb773e9e8c646f9ca1b7846b84ea6c
EF237 <jjamesrollins237@utexas.edu>
2025-01-30 18:35:07 -0600

    update

--- 2025-01-08-IN.CLASS.WORK.R
+++ 2025-01-08-IN.CLASS.WORK.R
@@ -1,5 +1,9 @@
-f <- file.choose()
-d <- read.csv(f)
+library(tidyverse)
+
+
+ f <- file.choose()
+# f <- "https://raw.githubusercontent.com/difiore/ada-datasets/refs/heads/main/Country-Data-2016.csv"
+d <- read_csv(f, col_names = TRUE)
 d
 
 class(d)
@@ -12,7 +16,20 @@ d$country
 names(d)
 meidan(d$population, na.rm = TRUE)
 median(d$area, na.rm = TRUE)
+
 d$density <- d$population/d$area
 
-order(d$density)
-sort
+v <- sort(d$density, decreasing = TRUE)
+
+d <- d %>% arrange(desc(density))
+
+str(d)
+structure(d)
+
+d$country
+
+d[ ,1]
+
+structure(d)
+
+d <- as.data.frame(d)
